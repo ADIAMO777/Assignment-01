4bd89ea3467deb775954fee09fe5be0248663eb9
Afolabi <afolabiadeolaolajumoke@gmail.com>
2025-11-25 17:28:47 +0100

    commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const btn = document.getElementById('hamburger');
 const navlinks = document.getElementById('nav-links');
 
 btn.addEventListener('click', () => {
-  btn.classList.toggle('active');   // animate hamburger
-  navlinks.classList.toggle('show'); // show/hide menu
+    btn.classList.toggle('active');
+    navlinks.classList.toggle('show');
 });
-
